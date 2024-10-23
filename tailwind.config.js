caa14476ee883fbdc68df552cabbea3752804e58
Madan-57 <madhangnagu2004@gmail.com>
2024-10-23 20:59:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config={
+    theme:{
+        gridTemplateColumns:{
+            'auto':'repeat(auto-fit, minmax(200px, 1fr))'
+        },
+        animation:{
+            spin_slow: 'spin 6s liner infinite'
+        }
+    }
+}
+
